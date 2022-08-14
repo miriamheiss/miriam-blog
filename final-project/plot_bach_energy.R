6df5a6046caeed29bfe23ed03e7154557f5b1243
miriamheiss <memerheiss@gmail.com>
2022-08-14 11:16:47 -0400

    Fixed a duplicate file, made a direct path for rds

--- final-project/plot_bach_energy.R
+++ final-project/plot_bach_energy.R
@@ -7,11 +7,13 @@ bach_graph <- bach_audio_clean %>%
   geom_point(size = 2.5, alpha = 0.75) + 
   scale_color_manual(values = c("lightpink3",
                                 "paleturquoise4",
-                                "thistle4"))+ 
-  scale_y_continuous(breaks = c(0.0, 0.25, 0.50, 0.75, 1.00)) + 
+                                "thistle4"),
+                     guide = guide_legend(ncol = 2, 
+                                          title.position = "top"))+
+   scale_y_continuous(breaks = c(0.0, 0.25, 0.50, 0.75, 1.00)) + 
   coord_cartesian(ylim = c(0.0, 1.00))+ 
-  theme_linedraw(base_size = 10)+
-  theme(axis.text.x = element_text(angle = 90), legend.position = "right")+ 
+  theme_linedraw(base_size = 12)+
+  theme(axis.text.x = element_text(angle = 90), legend.position = "bottom")+ 
   labs(title = "Energy by Key (J. S. Bach)", 
        x = "Key", 
        y = "Energy",
@@ -20,3 +22,4 @@ bach_graph <- bach_audio_clean %>%
   annotate(geom = "label", x = "A# minor", y = 1, label = "High")
 
 write_rds(bach_graph, "bach_energy_plot.rds")
+
--- final-project/plot_ts_energy.R
+++ final-project/plot_ts_energy.R
@@ -13,16 +13,19 @@ ts_graph<- ggplot(data = ts_audio_clean,
                                 "darkred",
                                 "grey20",
                                 "orchid4",
-                                "palegreen4"))+ 
+                                "palegreen4"),
+                     guide = guide_legend(ncol = 2, 
+                                          title.position = "top"))+ 
   labs(title = "Energy by Key (Taylor Swift)",
        y = "Energy",
        x = "Key", 
        color = "Album")+ 
   annotate(geom = "label", x = "A# minor", y = 0, label = "Low")+ 
   annotate(geom = "label", x = "A# minor", y = 1, label = "High")+ 
-  theme_linedraw(base_size = 10)+ 
-  theme(axis.text.x = element_text(angle = 90), legend.position = "left")
+  theme_linedraw(base_size = 12)+ 
+  theme(axis.text.x = element_text(angle = 90), legend.position = "bottom")
 
 #ggplotly(ts_graph, tooltip = "text") this is optional for final project lol
 
-write_rds(ts_graph, "ts_energy_plot.rds")
+write_rds(ts_graph, "~/Documents/Projects/miriam-blog/final-project/ts_energy_plot.rds")
+
